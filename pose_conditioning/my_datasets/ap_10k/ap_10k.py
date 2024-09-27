4c3a507f2e95e4a2efb00dae7a7195c303d6fb12
David Carlyn <davidecarlyn@gmail.com>
2024-09-27 16:48:04 -0400

    updated conditioning skeleton color

--- pose_conditioning/my_datasets/ap_10k/ap_10k.py
+++ pose_conditioning/my_datasets/ap_10k/ap_10k.py
@@ -11,6 +11,67 @@ from PIL import Image
 import datasets
 
 AP10K_ROOT_DIR = Path("/local/scratch/carlyn.1/datasets/ap-10k/")
+SPLIT = 1
+
+
+def get_colors():
+    ap10k_kpt_colors = {
+        "left_eye": (255, 0, 255),
+        "right_eye": (170, 0, 255),
+        "nose": (255, 0, 0),
+        "neck": (255, 85, 0),
+        "root_of_tail": (41, 163, 129),  # Custom!
+        "left_shoulder": (85, 255, 0),
+        "left_elbow": (0, 255, 0),
+        "left_front_paw": (0, 255, 85),  # In place of left wrist
+        "right_shoulder": (255, 170, 0),
+        "right_elbow": (255, 255, 0),
+        "right_front_paw": (170, 255, 85),  # In place of right wrist
+        "left_hip": (0, 85, 255),
+        "left_knee": (0, 0, 255),
+        "left_back_paw": (85, 0, 255),  # In place of left ankle
+        "right_hip": (0, 85, 255),
+        "right_knee": (0, 255, 255),
+        "right_back_paw": (0, 170, 255),  # In place of right ankle
+    }
+
+    ap10k_link_colors = {
+        (1, 2): (153, 0, 153),
+        (1, 3): (153, 0, 102),
+        (2, 3): (102, 0, 153),
+        (3, 4): (0, 0, 153),  # Neck
+        (4, 5): (100, 100, 100),  # Back
+        (4, 6): (153, 51, 0),  # Left shoulderblade
+        (6, 7): (102, 153, 0),  # Left arm
+        (7, 8): (51, 153, 0),  # Left forearm
+        (4, 9): (153, 0, 0),  # Right shoulderblade
+        (9, 10): (153, 102, 0),  # Right arm
+        (10, 11): (153, 153, 0),  # Right forearm
+        (5, 12): (0, 153, 153),  # Left upper leg
+        (12, 13): (0, 102, 153),  # Left lower leg
+        (13, 14): (0, 51, 153),  # Left ankle/foot
+        (5, 15): (0, 153, 0),  # right upper leg
+        (15, 16): (0, 153, 51),  # right lower leg
+        (16, 17): (0, 153, 102),  # right ankle/foot
+    }
+
+    ann_path = AP10K_ROOT_DIR / f"annotations/ap10k-train-split{SPLIT}.json"
+    with open(ann_path, "r") as f:
+        ann_data = json.load(f)
+
+    keypoint_names = ann_data["categories"][0][
+        "keypoints"
+    ]  # names are the same across all animals (categories)
+    skeleton_links = ann_data["categories"][0][
+        "skeleton"
+    ]  # skeltons are the same across all animals (categories)
+
+    kpt_colors = [tuple(list(ap10k_kpt_colors[name])) for name in keypoint_names]
+    link_colors = [
+        tuple(list(ap10k_link_colors[tuple(link)])) for link in skeleton_links
+    ]
+
+    return kpt_colors, link_colors
 
 
 def collect_and_sort_classnames(ann_data):
@@ -35,7 +96,7 @@ def collect_and_sort_skeletons(ann_data):
 
 
 def get_ap10k_classnames(root_path):
-    ann_path = root_path / "annotations/ap10k-train-split1.json"
+    ann_path = root_path / f"annotations/ap10k-train-split{SPLIT}.json"
     with open(ann_path, "r") as f:
         ann_data = json.load(f)
         class_names = collect_and_sort_classnames(ann_data)
@@ -120,9 +181,13 @@ class AP10K(datasets.GeneratorBasedBuilder):
 
             return data
 
-        train_data = collect(AP10K_ROOT_DIR / "annotations/ap10k-train-split1.json")
-        val_data = collect(AP10K_ROOT_DIR / "annotations/ap10k-val-split1.json")
-        test_data = collect(AP10K_ROOT_DIR / "annotations/ap10k-test-split1.json")
+        train_data = collect(
+            AP10K_ROOT_DIR / f"annotations/ap10k-train-split{SPLIT}.json"
+        )
+        val_data = collect(AP10K_ROOT_DIR / f"annotations/ap10k-val-split{SPLIT}.json")
+        test_data = collect(
+            AP10K_ROOT_DIR / f"annotations/ap10k-test-split{SPLIT}.json"
+        )
 
         return [
             datasets.SplitGenerator(
@@ -139,11 +204,13 @@ class AP10K(datasets.GeneratorBasedBuilder):
             ),
         ]
 
-    def create_cond_img(self, id, img_shape, kpts, kptv, skeleton):
+    def create_cond_img(
+        self, id, img_shape, kpts, kptv, skeleton, kpt_colors, link_colors
+    ):
         image = np.zeros(img_shape)
 
         pose_local_visualizer = PoseLocalVisualizer(
-            radius=8, line_width=3, link_color="green"
+            radius=8, line_width=3, link_color=link_colors, kpt_color=kpt_colors
         )
         gt_instances = InstanceData()
         gt_instances.keypoints = np.array([kpts])
@@ -167,6 +234,8 @@ class AP10K(datasets.GeneratorBasedBuilder):
         keypoints,
         keypoint_visible,
     ):
+        kpt_colors, link_colors = get_colors()
+
         for id, label, cls_name, skeleton, filepath, kpts, kptv in zip(
             ids,
             class_idx,
@@ -178,7 +247,7 @@ class AP10K(datasets.GeneratorBasedBuilder):
         ):
             img = load_image(filepath)
             cond_img = self.create_cond_img(
-                id, np.array(img).shape, kpts, kptv, skeleton
+                id, np.array(img).shape, kpts, kptv, skeleton, kpt_colors, link_colors
             )
 
             caption = f"A photo of a {cls_name}"
